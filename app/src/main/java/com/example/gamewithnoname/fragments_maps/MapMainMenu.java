f351c34bfc2db3ecb512127e55c60eda6947a2f0
Aleksey Piskunov <piskunov.alesha@gmail.com>
2019-05-30 04:08:21 +0700

    [+]two maps with my_locate, but first work badly

---
+++ app/src/main/java/com/example/gamewithnoname/fragments_maps/MapMainMenu.java
@@ -0,0 +1,80 @@
+package com.example.gamewithnoname.fragments_maps;
+
+import android.location.Location;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gamewithnoname.R;
+import com.example.gamewithnoname.UserLocation;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapMainMenu extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private final String TAG = String.format("%s/%s",
+            "HITS", "MapMainMenu");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        Log.i(TAG, "run onCreate");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.i(TAG, "run onCreateView");
+        View rootView = inflater.inflate(R.layout.map_holder, container, false);
+        final MapView mapView = rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return rootView;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.i(TAG, "onMapReady");
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        Location location = UserLocation.imHere;
+        double a = location.getLatitude();
+        double b = location.getLongitude();
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+//        mMap.setMyLocationEnabled(true);
+
+        LatLng place = new LatLng(a, b);
+        mMap.addMarker(new MarkerOptions().position(place).title("Current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
+        mMap.animateCamera(CameraUpdateFactory
+                .newLatLngZoom(place, 12.0f));
+    }
+}
