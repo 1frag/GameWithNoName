31238ce96d271e33b69c944744bb896a89630970
rozanovasya <rozanovasya@gmail.com>
2019-05-29 22:21:32 +0700

    some design, just first version with not working map in main activity

--- app/src/main/java/com/example/gamewithnoname/MainActivity.java
+++ app/src/main/java/com/example/gamewithnoname/MainActivity.java
@@ -5,17 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.maps.GoogleMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 
     public void processButtonPressing(View view) {
         switch (view.getId()) {
-            case R.id.button_start: {
+            case R.id.buttonStart: {
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
             }
