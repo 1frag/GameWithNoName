28f6de7220bb9e654bfec0ea7403232f57eb720c
Aleksey Piskunov <piskunov.alesha@gmail.com>
2019-06-03 00:42:16 +0700

    removed redundant old code

--- app/src/main/java/com/example/gamewithnoname/ServerConnection/ConnectionServer.java
+++ app/src/main/java/com/example/gamewithnoname/ServerConnection/ConnectionServer.java
@@ -19,15 +19,6 @@ public class ConnectionServer {
     private String result = "-1";
 
     public String fetchServerResult(String name, String password, @Nullable final LoginCallbacks callbacks) {
-//        //Obtain an instance of Retrofit by calling the static method.
-//        Retrofit retrofit = NetworkClient.getRetrofitClient();
-//        /*
-//        The main purpose of Retrofit is to create HTTP calls from the Java interface based on the annotation associated with each method. This is achieved by just passing the interface class as parameter to the create method
-//        */
-//        ServerAPIs serverAPIs = retrofit.create(ServerAPIs.class);
-//        /*
-//        Invoke the method corresponding to the HTTP request which will return a Call object. This Call object will used to send the actual network request with the specified parameters
-//        */
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -37,21 +28,17 @@ public class ConnectionServer {
 
         ServerAPIs serverAPIs = retrofit.create(ServerAPIs.class);
 
-        Call<ServerResponse> call = serverAPIs.getResultLogin(
+        Call call = serverAPIs.getResultLogin(
                 name,
                 password
         );
 
-        /*
-        This is the line which actually sends a network request. Calling enqueue() executes a call asynchronously. It has two callback listeners which will invoked on the main thread
-        */
         call.enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) {
                 /*This is the success callback. Though the response type is JSON, with Retrofit we get the response in the form of WResponse POJO class
                  */
                 if (response.body() != null) {
-//                    ServerResponse serverResponse = response.body();
                     Log.i(TAG, ((ServerResponse) response.body()).getResult());
                     result = ((ServerResponse) response.body()).getResult();
                 }
