eacb0348e85d428dcc952b652a8a24a5cbb60822
rozanovasya <rozanovasya@gmail.com>
2019-06-19 12:13:43 +0700

    minor fixes

--- app/src/main/java/com/example/gamewithnoname/activities/FriendsActivity.java
+++ app/src/main/java/com/example/gamewithnoname/activities/FriendsActivity.java
@@ -2,6 +2,7 @@ package com.example.gamewithnoname.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.gamewithnoname.R;
 
@@ -11,5 +12,8 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+    }
+    public void backToMain(View view) {
+        finish();
     }
 }
